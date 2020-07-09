cc8542f00be4e1eb22f7b8ddfc930954e5382cb1
huanglianqi <huanglianqi@users.noreply.github.com>
2020-07-09 17:22:19 +0800

    v1.4.4 proxy for download image

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -10,7 +10,17 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // download image
+      'upload': {
+        target: 'https://www.rici.org.cn/',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/upload': ''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
