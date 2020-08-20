db584e7b70c9eaa2e9f44c4adb0f468f243f9d0b
huanglianqi <huanglianqi@users.noreply.github.com>
2020-08-20 13:47:10 +0800

    parts组件文件更新

---
+++ frontend/src/components/parts/NumberBoard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    id="numberBoard">
+    <div
+      v-b-hover="handleHover">
+      <b-button
+        class="p-5 shadow-sm"
+        block
+        variant="light">
+        <h1
+          :class="numberFrontClass"
+          v-show="!isHover">{{numberFront}}</h1>
+        <h1
+          :class="numberBackClass"
+          v-show="isHover">{{numberBack}}</h1>
+        <p
+          class="font-weight-bold"
+          v-show="!isHover">
+          {{titleFront}}</p>
+        <p
+          class="font-weight-bold"
+          v-show="isHover">
+          {{titleBack}}</p></b-button></div></div>
+</template>
+
+<script>
+export default {
+  name: 'numberBoard',
+  props: {
+    titleFront: {
+      type: String,
+      required: false
+    },
+    titleBack: {
+      type: String,
+      required: false
+    },
+    numberFront: {
+      type: String,
+      required: false
+    },
+    numberBack: {
+      type: String,
+      required: false
+    },
+    numberFrontClass: {
+      type: String,
+      required: false
+    },
+    numberBackClass: {
+      type: String,
+      required: false
+    }
+  },
+  data () {
+    return {
+      isHover: false
+    }
+  },
+  methods: {
+    handleHover (hovered) {
+      this.isHover = hovered
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/parts/ToolDropdown.vue
@@ -0,0 +1,107 @@
+<template>
+  <div
+    id="toolDropdown">
+    <b-dropdown
+      toggle-class="shadow-sm border-white border-0 p-2"
+      block
+      no-caret
+      menu-class="w-100"
+      :variant="variant">
+      <template
+        v-slot:button-content>
+        <div
+          v-on:click="toolDropdownDown=true">
+          <b-icon
+            :icon="iconUp"
+            shift-v="-1"
+            class="float-left mr-2"
+            v-show="!toolDropdownDown"></b-icon>
+          <b-icon
+            :icon="iconDown"
+            shift-v="-1"
+            class="float-left mr-2"
+            v-show="toolDropdownDown"></b-icon>
+          {{title}}
+          <b-icon
+            class="float-right ml-1"
+            shift-v="-1"
+            icon="three-dots-vertical"
+            v-show="!toolDropdownDown"></b-icon>
+          <b-icon
+            class="float-right ml-1"
+            icon="x"
+            v-show="toolDropdownDown"></b-icon></div></template>
+      <b-dropdown-item
+        v-on:click="selectItem(firstItemTitle, firstItemValue)">
+        {{firstItemTitle}}
+      </b-dropdown-item>
+      <b-dropdown-divider></b-dropdown-divider>
+      <b-dropdown-item
+        v-for="item in itemList"
+        v-bind:key="item.title"
+        v-on:click="selectItem(item.title, item.value)">
+        {{item.title}}</b-dropdown-item></b-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toolDropdown',
+  props: {
+    variant: {
+      type: String,
+      required: true
+    },
+    iconUp: {
+      type: String,
+      required: true
+    },
+    iconDown: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: false
+    },
+    firstItemTitle: {
+      type: String,
+      required: false
+    },
+    firstItemValue: {
+      type: String,
+      required: false
+    },
+    itemList: {
+      type: Array,
+      required: false
+    },
+    selectItemFunc: {
+      type: Function,
+      required: true
+    }
+  },
+  data () {
+    return {
+      toolDropdownDown: false
+    }
+  },
+  mounted () {
+    this.$root.$on(
+      'bv::dropdown::hide',
+      () => {
+        this.toolDropdownDown = false
+      }
+    )
+  },
+  methods: {
+    selectItem (title, value) {
+      this.selectItemFunc(title, value)
+    }
+  }
+}
+</script>
